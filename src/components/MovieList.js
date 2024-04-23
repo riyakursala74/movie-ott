6091619e59b4729cf07ab11eb74c55da0b9002ca
riyakursala74 <riyak7492@gmail.com>
2024-04-23 22:36:21 +0530

    dynamic scroll

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,31 +1,26 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
-const scrollRight = () => {
-  document.getElementById("content").scrollBy(100, 0);
+const scrollRight = (id) => {
+  document.getElementById(id).scrollBy(100, 0);
 };
 
-const scrollLeft = () => {
-  const content = document.getElementById("content");
-  if (
-    Math.abs(content.scrollLeft) ===
-    content.scrollWidth - content.clientWidth
-  ) {
-  }
-  content.scrollBy(-100, 0);
+const scrollLeft = (id) => {
+  document.getElementById(id).scrollBy(-100, 0);
 };
-const MovieList = ({ movies, title }) => {
+
+const MovieList = ({ movies, title, id }) => {
   return (
     <div className="md:pl-6 pl-2">
       <div className="text-white md:text-xl italic">{title}</div>
       <div className="flex">
         <div
           className="relative h-64 mt-6 w-8 left-0 hidden md:block"
-          onClick={scrollLeft}
+          onClick={() => scrollLeft(id)}
         >
           <FaCaretLeft className="text-white font-bold text-xl bg-gradient-to-b from:black items-center mt-24 " />
         </div>
-        <div className="flex overflow-x-scroll py-3 overflow-y-hidden">
+        <div className="flex overflow-x-scroll py-3 overflow-y-hidden" id={id}>
           {movies &&
             movies.map((movie) => {
               return <MovieCard movie={movie} key={movie.id} />;
@@ -33,7 +28,7 @@ const MovieList = ({ movies, title }) => {
         </div>
         <div
           className="relative p-2 right-0 h-64 mt-6 w-8  hidden md:block"
-          onClick={scrollRight}
+          onClick={() => scrollRight(id)}
         >
           <FaCaretRight className="text-white font-bold text-xl bg-gradient-to-b from:black items-center mt-24" />
         </div>
--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -7,14 +7,15 @@ const MoviesContainer = () => {
   const popular_movies = useSelector((store) => store.movie.popular_movies);
   const top_rated = useSelector((store) => store.movie.top_rated);
   const upcoming = useSelector((store) => store.movie.upcoming_movies);
+
   return (
     <div className=" bg-black md:pl-3">
       <div className="md:-mt-44 relative z-40">
-        <MovieList movies={now_movies} title="Now Playing" />
+        <MovieList movies={now_movies} title="Now Playing" id="0" />
       </div>
-      <MovieList movies={top_rated} title="Top Rated" />
-      <MovieList movies={upcoming} title="Upcoming" />
-      <MovieList movies={popular_movies} title="Popular" />
+      <MovieList movies={top_rated} title="Top Rated" id="1" />
+      <MovieList movies={upcoming} title="Upcoming" id="2" />
+      <MovieList movies={popular_movies} title="Popular" id="3" />
     </div>
   );
 };
